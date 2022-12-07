56be98de0d5bb4ef7ad70a772b66fb5e159251d3
Linh_Nguyen <kaminguyen@hotmail.com>
2022-12-06 20:34:56 -0500

    Small fixing

--- Lab1/Models/CustomResponseHeaderMiddleware.cs
+++ Lab1/Models/CustomResponseHeaderMiddleware.cs
@@ -21,7 +21,6 @@ namespace Lab1.Models
                 httpContext.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000"); //Strict-Transport-Security
                 httpContext.Response.Headers.Add("X-Content-Type-Options", "nosniff");  //X-Content-Type-Options
                 httpContext.Response.Headers.Add("X-Xss-Protection", "1; mode=block"); //X-Xss-Protection
-                                                                                       //httpContext.Response.Headers.Add("Content-Security-Policy", "default-src 'self'"); //Content-Security-Policy - getting error when added this
                 //Permissions-Policy
                 httpContext.Response.Headers.Add("Permissions-Policy", "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()");
                 httpContext.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN"); //HeadersX - Frame - Options
